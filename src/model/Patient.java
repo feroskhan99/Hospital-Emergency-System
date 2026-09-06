c16662fd402c950f92cb76351d991c15813ed141
feroskhan99 <abdullahferos5@gmail.com>
2026-09-06 21:41:26 +0530

    Added Patient model class

---
+++ src/model/Patient.java
@@ -0,0 +1,45 @@
+package model;
+
+public class Patient {
+
+private int patientId;
+private String name;
+private int age;
+private String contactNumber;
+private String medicalCondition;
+
+public Patient(int patientId, String name, int age, String contactNumber, String medicalCondition) {
+    this.patientId = patientId;
+    this.name = name;
+    this.age = age;
+    this.contactNumber = contactNumber;
+    this.medicalCondition = medicalCondition;
+}
+public int getPatientId() {
+    return patientId;
+}
+
+public String getName() {
+    return name;
+}
+
+public int getAge() {
+    return age;
+}
+
+public String getContactNumber() {
+    return contactNumber;
+}
+
+public String getMedicalCondition() {
+    return medicalCondition;
+}
+public void displayDetails() {
+    System.out.println("Patient ID: " + patientId);
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("Contact Number: " + contactNumber);
+    System.out.println("Medical Condition: " + medicalCondition);
+    System.out.println("-----------------------------");
+}
+}
